c454b0e3b5eff417a60b8a33fc0ed69ccd696204
databj <benjiable21mail.com>
2021-05-12 13:08:28 -0500

    vistas de usuarios

--- core/app/view/Card-view.php
+++ core/app/view/Card-view.php
@@ -2,9 +2,49 @@
 <html lang="zxx">
    
 <div id="main-wrapper">
-         
+     <!-- Badges Alerts Start -->
+                     <!--================================-->							
+                     <div class="col-md-12 col-lg-12">
+                        <div class="card mg-b-20">
+                           <div class="card-header">
+                              <h4 class="card-header-title">
+                               <center> Historial De Trabajos
+                              </h4>
+                              
+                           </div>
+                           <div class="card-body collapse show" id="collapse3">
+                           <?php 
+                           $procesos=ProcesoData::getAll();
+                           foreach ($procesos as $key => $procesos) {
+                              
+                           if($procesos->id_empresa==299 && $procesos->estado!= "FINALIZADO"){
+                              $placa=EquipoData::getById($procesos->id_equipo);
+                              $Tempario=TemparioData::getById($procesos->tipo_trabajo);
 
-          
+                           
+                           ?>
+                              <div class="alert alert-primary fade show" role="alert">
+                                <?php echo $placa->placa."  ".$Tempario->descripcion; ?>
+                                 <a class="badge badge-success pull-right">
+
+                                 <form action="index.php?view=Historial" method="post">
+                                 <input type="hidden" name="id" value=<?php echo $procesos->id;?>>
+                                 <input type="hidden" name="view" value=<?php echo $_GET["view"];?>>
+                                 <button class="dropdown-item" type="submit"><i class="fa fa-eye"></i> Ver Historial</button>  
+                                 </form>
+                                 </a>
+
+                              </div>
+                            
+                            <?php 
+                           }
+                           }
+                            
+                            ?>
+                           </div>
+                        </div>
+                     </div>
+                     <!-- / Badges Alerts End -->
                 
 </div>
 
---
+++ core/app/view/Historial-view.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   
+<div id="main-wrapper">
+     <!-- Badges Alerts Start -->
+                     <!--================================-->							
+                     <div class="col-md-12 col-lg-12">
+                        <div class="card mg-b-40">
+                           <div class="card-header">
+                              <h4 class="card-header-title">
+                               <center> Historial De Trabajos
+                              </h4>
+                              
+                           </div>
+                           <div class="card-body collapse show" id="collapse3">
+                           <?php 
+                           $diagnostico=DiagnosticoData::getByIdProceso($_POST["id"]);
+                           $proceso=ProcesoData::getById($_POST["id"]);
+                           foreach ($diagnostico as $key => $diagnostico) {
+                              
+                              $placa=EquipoData::getById($proceso->id_equipo);
+                              $Tempario=TemparioData::getById($diagnostico->tipo_diagnostico);
+
+                           
+                           ?>
+                              <div class="alert alert-primary fade show" role="alert">
+                                <?php echo $placa->placa."  ".$Tempario->descripcion; ?>
+                                 <a class="badge badge-success pull-right">
+
+                                 <form action="index.php?view=Historial" method="post">
+                                 <input type="hidden" name="id" value=<?php echo $proceso->id;?>>
+                                 <input type="hidden" name="view" value=<?php echo $_GET["view"];?>>
+                                 <button class="dropdown-item" type="submit"><i class="fa fa-eye"></i> Ver Historial</button>  
+                                 </form>
+                                 </a>
+
+                              </div>
+                            
+                            <?php 
+                           
+                           }
+                            
+                            ?>
+                           </div>
+
+                           <div class="card-body collapse show" >
+                              <div class="activity">
+                                 <i class="icon-check bg-soft-primary"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Task finished</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-teal"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Video conference</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-warning"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Task Overdue</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-danger"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Added your friend list</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-success"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Task Overdue</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-primary"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Submit a blog</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-teal"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">New Request</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                                 <i class="icon-check bg-soft-warning"></i>
+                                 <div class="time-item">
+                                    <div class="item-info ">
+                                       <div class="d-flex justify-content-between align-items-center">
+                                          <h6 class="tx-dark tx-13 mb-0">Task Overdue</h6>
+                                          <span class="small">05:00 PM Sun, 02 Feb 2019</span>
+                                       </div>
+                                       <p class="mt-2 tx-12">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                       <div><span class="badge bg-soft-primary tx-uppercase">Design</span> <span class="badge bg-soft-danger  tx-uppercase">HTML</span> <span class="badge bg-soft-success  tx-uppercase">Css</span> <span class="badge bg-soft-teal  tx-uppercase">Dashboard</span></div>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+
+
+                      
+                           
+                        
+                    
+                     <!-- / Badges Alerts End -->
+                
+</div>
+
+</html>
