26a8412f69bb145c2e4cbe41f12485dc12bf3f03
databj <benjiable21@gmail.com>
2021-06-21 14:03:02 -0500

    mecgo historial

--- core/app/view/Card-view.php
+++ core/app/view/Card-view.php
@@ -1,51 +1,66 @@
-<!DOCTYPE html>
-<html lang="zxx">
-   
-<div id="main-wrapper">
-     <!-- Badges Alerts Start -->
-                     <!--================================-->							
-                     <div class="col-md-12 col-lg-12">
-                        <div class="card mg-b-20">
-                           <div class="card-header">
-                              <h4 class="card-header-title">
-                               <center> Historial De Trabajos
-                              </h4>
-                              
-                           </div>
-                           <div class="card-body collapse show" id="collapse3">
-                           <?php 
-                           $procesos=ProcesoData::getAll();
-                           foreach ($procesos as $key => $procesos) {
-                              
-                           if($procesos->id_empresa==299 && $procesos->estado!= "FINALIZADO"){
-                              $placa=EquipoData::getById($procesos->id_equipo);
-                              $Tempario=TemparioData::getById($procesos->tipo_trabajo);
+<div class="col-md-12 col-lg-12">
+    <div class="card mg-b-20">
+        <div class="card-header">
+            <h4 class="card-header-title">
+              buscar trabajos
+            </h4>
+            <div class="card-header-btn">
+                <a href="#" data-toggle="collapse" class="btn card-collapse" data-target="#collapse4" aria-expanded="true"><i class="ion-ios-arrow-down"></i></a>
+                <a href="#" data-toggle="refresh" class="btn card-refresh"><i class="ion-android-refresh"></i></a>
+                <a href="#" data-toggle="expand" class="btn card-expand"><i class="ion-android-expand"></i></a>
+                <a href="#" data-toggle="remove" class="btn card-remove"><i class="ion-android-close"></i></a>
+            </div>
+        </div>
 
-                           
-                           ?>
-                              <div class="alert alert-primary fade show" role="alert">
-                                <?php echo "Ver Trabajos para Vehiculo  --->  ".$placa->placa ?>
-                                 <a class="badge badge-success pull-right">
 
-                                 <form action="index.php?view=Historial" method="post">
-                                 <input type="hidden" name="id" value=<?php echo $procesos->id;?>>
-                                
-                                 <button class="dropdown-item" type="submit"><i class="fa fa-eye"></i> Ver Historial</button>  
-                                 </form>
-                                 </a>
 
-                              </div>
-                            
-                            <?php 
-                           }
-                           }
-                            
-                            ?>
-                           </div>
+
+        <div class="card-body collapse show" id="collapse4">
+
+
+            <form class="form-horizontal" method="post" id="addproduct" action="index.php?view=Trabajos" role="form">
+
+                <input type="hidden" id="View" name="View" value="<?php echo $_GET["view"]; ?>">
+
+                <div class="row">
+
+
+
+
+                    <div class="col-md-6 mb-3">
+                        <p>Id</p>
+                        <div class="input-group mb-6">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon3"><i class="fa fa-user"></i></span>
+                            </div>
+                            <input type="text" class="form-control" name="id" id="id" placeholder="" aria-label="Username" aria-describedby="basic-addon3" required>
                         </div>
-                     </div>
-                     <!-- / Badges Alerts End -->
-                
-</div>
 
-</html>
+                    </div>
+
+
+
+
+
+
+
+
+
+
+                    <div class="col-md-6 mb-3">
+                    <p>Confirme</p>
+                        <button class="btn btn-custom-primary" onclick="return pregunta()" type="submit">Confirmar</button>
+                    </div>
+
+
+
+
+
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+</div>
---
+++ core/app/view/Trabajos-view.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<div id="main-wrapper">
+    <!-- Badges Alerts Start -->
+    <!--================================-->
+    <div class="col-md-12 col-lg-12">
+        <div class="card mg-b-20">
+            <div class="card-header">
+                <h4 class="card-header-title">
+                    <center> Historial De Trabajos
+                </h4>
+
+            </div>
+            <div class="card-body collapse show" id="collapse3">
+                <?php
+                $procesos = ProcesoData::getAll();
+                foreach ($procesos as $key => $procesos) {
+                    if (isset($_POST["id"])) {
+
+
+                        if ($procesos->id_empresa == $_POST["id"] && $procesos->estado != "FINALIZADO") {
+                            $placa = EquipoData::getById($procesos->id_equipo);
+                            $Tempario = TemparioData::getById($procesos->tipo_trabajo);
+
+
+                ?>
+                            <div class="alert alert-primary fade show" role="alert">
+                                <?php echo "Ver Trabajos para Vehiculo  --->  " . $placa->placa ?>
+                                <a class="badge badge-success pull-right">
+
+                                    <form action="index.php?view=Historial" method="post">
+                                        <input type="hidden" name="id" value=<?php echo $procesos->id; ?>>
+
+                                        <button class="dropdown-item" type="submit"><i class="fa fa-eye"></i> Ver Historial</button>
+                                    </form>
+                                </a>
+
+                            </div>
+
+                <?php
+                        }
+                    }
+
+                    if (isset($_GET["id"])) {
+
+
+                        if ($procesos->id_empresa == $_GET["id"] && $procesos->estado != "FINALIZADO") {
+                            $placa = EquipoData::getById($procesos->id_equipo);
+                            $Tempario = TemparioData::getById($procesos->tipo_trabajo);
+
+
+                ?>
+                            <div class="alert alert-primary fade show" role="alert">
+                                <?php echo "Ver Trabajos para Vehiculo  --->  " . $placa->placa ?>
+                                <a class="badge badge-success pull-right">
+
+                                    <form action="index.php?view=Historial" method="post">
+                                        <input type="hidden" name="id" value=<?php echo $procesos->id; ?>>
+
+                                        <button class="dropdown-item" type="submit"><i class="fa fa-eye"></i> Ver Historial</button>
+                                    </form>
+                                </a>
+
+                            </div>
+
+                <?php
+                        }
+                    }
+
+
+                }
+
+                ?>
+            </div>
+        </div>
+    </div>
+    <!-- / Badges Alerts End -->
+
+</div>
+
+</html>
