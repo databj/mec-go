a3517f28f3eafe3af218604014960709348fdc3b
databj <benjiable21@gmail.com>
2021-07-11 14:12:15 -0500

    mejorando la busqueda

--- core/app/view/Trabajos-view.php
+++ core/app/view/Trabajos-view.php
@@ -14,7 +14,11 @@
             </div>
             <div class="card-body collapse show" id="collapse3">
                 <?php
-                $procesos = ProcesoData::getAll();
+                if (isset($_POST["id"])) {
+                    $procesos = ProcesoData::getByEmpresa($_POST["id"]);
+                } else {
+                    $procesos = ProcesoData::getByEmpresa($_GET["id"]);
+                }
                 foreach ($procesos as $key => $procesos) {
                     if (isset($_POST["id"])) {
 
@@ -38,7 +42,7 @@
 
                             </div>
 
-                <?php
+                        <?php
                         }
                     }
 
@@ -50,7 +54,7 @@
                             $Tempario = TemparioData::getById($procesos->tipo_trabajo);
 
 
-                ?>
+                        ?>
                             <div class="alert alert-primary fade show" role="alert">
                                 <?php echo "Ver Trabajos para Vehiculo  --->  " . $placa->placa ?>
                                 <a class="badge badge-success pull-right">
@@ -67,15 +71,30 @@
                 <?php
                         }
                     }
-
-
                 }
 
                 ?>
+
+                <!--------------- <div class="alert alert-danger alert-bordered pd-y-15" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true"><i class="ico icon-close"></i></span>
+                    </button>
+                    <div class="d-sm-flex align-items-center justify-content-start">
+                        <i class="icon ion-ios-close alert-icon tx-52 tx-danger mg-r-20"></i>
+                        <div class="mg-t-20 mg-sm-t-0">
+                            <h5 class="mg-b-2 tx-danger">Oh snap! Change a few things up.</h5>
+                            <p class="mg-b-0 tx-gray">Neque porro quisquam est, qui consectetur, adipisci velit...</p>
+                        </div>
+                    </div>
+                </div>-->
+
+
             </div>
         </div>
     </div>
     <!-- / Badges Alerts End -->
+
+
 
 </div>
 
